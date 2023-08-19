05884223fa1852338889c03fe6aa67595483d5d0
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2023-08-19 12:17:58 +0600

    Update Day 2_ C++ Basic.cpp

--- Practice/Day 2_ C++ Basic.cpp
+++ Practice/Day 2_ C++ Basic.cpp
@@ -5,7 +5,16 @@ int main()
 {
     // precedence  of data type : char< int < long int < long long int < float < double
     // full calculation on higher order
-    cout << 'c'+1;
-    
+    cout << 'c'+1 << endl;
+    int a=3/2;
+    cout<< a << endl;
+
+    // Oprator precendace print on internet
+
+    cout<< 7/2 *3 << endl;
+    cout << 3*7/2 << endl;
+
+
+
 
 }
