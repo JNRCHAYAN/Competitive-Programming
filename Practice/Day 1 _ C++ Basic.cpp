0dc67c1b627db7ec477fa2b6e27fc9d2ac3e1e39
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2023-08-19 11:50:56 +0600

    Update Day 1 _ C++ Basic.cpp

--- Practice/Day 1 _ C++ Basic.cpp
+++ Practice/Day 1 _ C++ Basic.cpp
@@ -1,8 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
-	cout<<"Hello world"<<endl;
-	return 0;
+	//print anything in c++ program
+
+	cout<<"Hello world"<< " "<<"My name is chayan"<<endl;
+
+	// Vatiable
+
+	int a;
+	a=10;
+	cout<<a<<endl;
+
+	// Data type
+	// char, int float, double, bool
+
+	char c= 'a';
+	int b=3;
+	double d=3.5;
+	bool f=1;
+	cout<< c << endl << b << endl << d << endl << f << endl;
 }
