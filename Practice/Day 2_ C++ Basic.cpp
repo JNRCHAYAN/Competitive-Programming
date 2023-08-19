75c3a4c9050e46b86df755c6799430263fc5b08e
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2023-08-19 12:12:57 +0600

    Create Day 2_ C++ Basic.cpp

---
+++ Practice/Day 2_ C++ Basic.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // precedence  of data type : char< int < long int < long long int < float < double
+    // full calculation on higher order
+    cout << 'c'+1;
+    
+
+}
