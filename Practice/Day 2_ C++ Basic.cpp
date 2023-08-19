70141ef5564cda55dfb2d3ad1bb5e9731e265cb2
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2023-08-19 12:26:38 +0600

    Update Day 2_ C++ Basic.cpp

--- Practice/Day 2_ C++ Basic.cpp
+++ Practice/Day 2_ C++ Basic.cpp
@@ -14,6 +14,31 @@ int main()
     cout<< 7/2 *3 << endl;
     cout << 3*7/2 << endl;
 
+    // Data type range and overflow Download throw in internet
+    // Data type overflow
+
+    int r = 100000;
+    int i = 100000;
+    int cal = r*i;
+    cout << cal << endl;
+    int mx = INT_MAX;
+    cout << mx << endl;
+    
+    // oberflow calculate using Long int Not work 
+     long int rr = 100000;
+     long int ii = 100000;
+     long int calo = rr*ii;
+    cout << calo << endl;
+
+     // oberflow calculate using Long Long int
+     long long int rrr = 100000;
+     long long int iii = 100000;
+     long long int callong = rrr*iii;
+    cout << callong << endl;
+    
+
+
+
 
 
 
