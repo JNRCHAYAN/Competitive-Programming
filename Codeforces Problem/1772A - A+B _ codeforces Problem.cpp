c059f8c906297009a92887ab72fcd49381dc2dc2
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2023-09-12 10:45:45 +0600

    Create 1772A - A+B _ codeforces Problem.cpp

---
+++ Codeforces Problem/1772A - A+B _ codeforces Problem.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a ,b,c, su=0;
+    cin >> a ;
+    for(int i=1; i<=a; i++)
+    {
+        cin >> b >> c;
+        su=b+c;
+        cout << su << endl; ;
+
+    }
+}
